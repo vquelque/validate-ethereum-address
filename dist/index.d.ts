078bd796597f93dcbeac9e047995846a68a9d368
val <valquel@pm.me>
2024-05-20 15:50:05 +0200

    format & compile

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,3 @@
 import { ValidInputTypes } from "./utils";
 export declare const checkAddressChecksum: (data: string) => boolean;
-export declare const isAddress: (
-  value: ValidInputTypes,
-  checkChecksum?: boolean,
-) => boolean;
+export declare const isAddress: (value: ValidInputTypes, checkChecksum?: boolean) => boolean;
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 export type ValidInputTypes = Uint8Array | bigint | string | number | boolean;
 
-const isHexStrict = (hex: ValidInputTypes) =>
+export const isHexStrict = (hex: ValidInputTypes) =>
   typeof hex === "string" && /^((-)?0x[0-9a-f]+|(0x))$/i.test(hex);
 
-function isUint8Array(data: unknown | Uint8Array): data is Uint8Array {
+export function isUint8Array(data: unknown | Uint8Array): data is Uint8Array {
   return (
     data instanceof Uint8Array ||
     (data as { constructor: { name: string } })?.constructor?.name ===
